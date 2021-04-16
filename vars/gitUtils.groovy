554f3bc7d0ce0027924b8a5fad0039853afd179a
Federico Virgili <federico.virgili@gmail.com>
2021-04-16 18:21:32 +0200

    checkout

--- vars/gitUtils.groovy
+++ vars/gitUtils.groovy
@@ -3,7 +3,30 @@ def checkout(branch) {
 
     script {
 
-        echo "checking out:${branch}"
+        try {
+
+            echo "checking out:${branch}"
+
+            sh "git branch --l"
+
+            sh "exit 1"
+
+        } catch (Exception e) {
+
+            throw new CheckoutException("error during checkout")
+
+        }
+
+    }
+
+}
+
+
+class CheckoutException extends Exception {
+
+    public CheckoutException(message) {
+
+        super(message)
 
     }
 
